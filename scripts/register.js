ebb71d8f22471c3aa79c6158a7f302b5a7211db3
philippthuemler <philipp.thuemler@t-online.de>
2022-03-11 11:26:33 +0100

    added register for multiple users

--- scripts/register.js
+++ scripts/register.js
@@ -1,8 +1,11 @@
 
-const namePattern = /([A-Z]|Ä|Ö|Ü)([a-z]|ä|ö|ü|ß)*/;
+const namePattern = /([A-Z]|Ä|Ö|Ü)([a-z]|ä|ö|ü|ß)+/;
 const usernamePattern = /([a-z]|[A-Z]|[0-9]|\.|-|_)*/;
 const emailPattern = /^([a-z]|[A-Z]|[0-9]|\.|_|-)*@([a-z]|[A-Z]|[0-9]|-)*\.([a-z]){2,3}$/;
 let err = document.getElementById('error-msg');
+err.style.color = 'red';
+let userCount = 0;
+let userObjName = 'user';
 
 function validateUserData(input, type) {
     switch(type) {
@@ -37,6 +40,7 @@ function validateUserData(input, type) {
 
 function validateUser(user) {
     let isUserValid = [];
+    let inputInvalidMsg = '';
     for (let [key, value] of Object.entries(user)) {
         if (validateUserData(value, key)) {
             console.log(`${key}: Ok`);
@@ -44,6 +48,11 @@ function validateUser(user) {
         } else {
             console.log(`${key}: error`);
             isUserValid.push(false);
+            if(inputInvalidMsg === '')
+                inputInvalidMsg += `${key} falsch`;
+            else
+                inputInvalidMsg += `, ${key} falsch`;
+            err.innerHTML = inputInvalidMsg;
         }
     }
     if(!isUserValid.includes(false))
@@ -71,8 +80,13 @@ function getUserData() {
 
 function storeUserData(user) {
     const userSerialized = JSON.stringify(user);
-    localStorage.setItem('user', userSerialized);
-    console.log(localStorage);
+    userCount++;
+    userObjName = userObjName.slice(0, 4);
+    userObjName += userCount;
+    if (!iterateUsers().includes(userObjName)) {
+        console.log(userObjName);
+        localStorage.setItem(userObjName, userSerialized);
+    }
 }
 
 function isUserInputEmpty(user) {
@@ -89,26 +103,36 @@ function isUserInputEmpty(user) {
     return false;
 }
 
-function getUserDataFromStorage() {
-    let user = localStorage.getItem('user');
+function getUserObject(userObjectName) {
+    let user = localStorage.getItem(userObjectName);
     user = JSON.parse(user);
-    console.log(user.firstname);
-    console.log(user.lastname);
-    console.log(user.username);
-    console.log(user.email);
-    console.log(user.password);
     return user;
 }
 
+function iterateUsers() {
+    let existingUsers = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        existingUsers.push(localStorage.key(i));
+    }
+    console.log(existingUsers);
+    return existingUsers;
+}
+
 function userExist(user) {
-    let storedUser = getUserDataFromStorage(user);
-    if (storedUser.username === user.username
-    || storedUser.email === user.email) {
-        console.log('User already exists');
-        err.innerHTML = 'Benutzer existiert bereits';
-        return true;
+    if(!(localStorage.length === 0)) {
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const userObject = getUserObject(key);
+            if(userObject.username === user.username || userObject.email === user.email) {
+                console.log('User already exists');
+                err.innerHTML = 'Benutzer existiert bereits';
+                return true;
+            }
+        }
+        console.log('User did not exist');
+        return false;
     } else {
-        console.log('User does not exist');
+        console.log('No users existed');
         return false;
     }
 }
@@ -118,11 +142,16 @@ function init() {
     const submitButton = document.getElementById('submit');
     submitButton.addEventListener('click', (event) => {
         event.preventDefault();
+        err.style.color = 'red';
         const user = getUserData();
         if (!isUserInputEmpty(user)) {
             if (validateUser(user))
-                if(!userExist(user))
+                if(!userExist(user)) {
                     storeUserData(user);
+                    err.style.color = 'green';
+                    err.innerHTML = 'Benutzer gespeichert';
+                }
+
         }
     }, false);
 }
