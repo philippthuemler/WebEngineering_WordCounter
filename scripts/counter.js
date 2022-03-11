58e18dd1f60389eb1b187b9d7a00468f651230fa
DamnDamoin <damion.haeberle@gmail.com>
2022-03-11 09:11:54 +0100

    removed sentences, let instead of var

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,24 +1,15 @@
-var input = document.querySelectorAll('textarea')[0],
+let input = document.querySelectorAll('textarea')[0],
     characterCount = document.querySelector('#characterCount'),
     wordCount = document.querySelector('#wordCount');
 
 input.addEventListener('keyup', function() {
     console.clear();
     characterCount.innerHTML = input.value.length;
-    var words = input.value.match(/\b[-?(\w+)?]+\b/gi);
+    let words = input.value.match(/\b[-?(\w+)?]+\b/gi);
     if (words) {
         wordCount.innerHTML = words.length;
     }
     else {
         wordCount.innerHTML = 0;
-    }
-
-    if (words) {
-        var sentences = input.value.split(/[.|!|?]+/g);
-        console.log(sentences);
-        sentenceCount.innerHTML = sentences.length - 1;
-    }
-    else {
-        sentenceCount.innerHTML = 0;
     }
 });
