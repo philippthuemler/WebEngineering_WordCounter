2eaf3888ec807221e26e239000d365345cf480ed
DamnDamoin <damion.haeberle@gmail.com>
2022-03-11 09:39:49 +0100

    fixed problems with Umlauts, maybe need for addition of extra symbols for word end.

--- scripts/counter.js
+++ scripts/counter.js
@@ -2,10 +2,12 @@ let input = document.querySelectorAll('textarea')[0],
     characterCount = document.querySelector('#characterCount'),
     wordCount = document.querySelector('#wordCount');
 
+const puncutations = [".", ",", ":", "!", "?", " "];
+
 input.addEventListener('keyup', function() {
     console.clear();
     characterCount.innerHTML = input.value.length;
-    let words = input.value.match(/\b[-?(\w+)?]+\b/gi);
+    let words = input.value.match(/\b[-?(\w+)?]+[".", ",", ":", "!", "?", " "]/gi);
     if (words) {
         wordCount.innerHTML = words.length;
     }
